855a10dc67d47158b0d95d117f0f06a8b48fd7dc
SIDDHARTH09-CMD <58885584+SIDDHARTH09-CMD@users.noreply.github.com>
2022-03-21 21:10:21 +0530

    First Commit

---
+++ build/precache-manifest.7b4d458b25b4bcd416fd267c9a7fc5ec.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca65425244e0324287649f7cc810609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ef4b5e76106d7c53023",
+    "url": "/static/js/2.4904155f.chunk.js"
+  },
+  {
+    "revision": "008209f1d9a526113b3d",
+    "url": "/static/js/main.d0f12995.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
